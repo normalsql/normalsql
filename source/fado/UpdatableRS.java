7da8ec310abfb5729dbb41883a521e33d2db2a8f
Jason Osgood <jason@jasonosgood.com>
2011-09-09 17:45:55 +0000

    Added IN condition support in Select.vm template.

--- source/fado/UpdatableRS.java
+++ source/fado/UpdatableRS.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class UpdatableRS 
+public class UpdatableRS
 {
-	public static void main( String[] args )
-		throws Exception
+	public static void main( String[] args ) throws Exception
 	{
 		String driver = "org.h2.Driver";
 		String url = "jdbc:h2:tcp://localhost/~/hydra";
@@ -18,46 +17,66 @@ public class UpdatableRS
 		Class.forName( driver );
 		Connection conn = DriverManager.getConnection( url, username, password );
 		conn.setAutoCommit( false );
-		Statement s1 = conn.createStatement(
-		        ResultSet.TYPE_SCROLL_SENSITIVE,
-		        ResultSet.CONCUR_UPDATABLE);
+		
+		
+		Statement s1 = conn.createStatement( ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE );
 
 		s1.executeQuery( "SELECT * FROM Patient" );
-	      ResultSet r1 = s1.getResultSet();
+		ResultSet r1 = s1.getResultSet();
 
-	      while (r1.next())
-	      {
-	        String middle = r1.getString( "MIDDLENAME" );
-	        System.out.println( "middle: " + middle );
-	        if( middle == null ) {
-	        	middle = "";
-	        }
+		while( r1.next() )
+		{
+			String middle = r1.getString( "MIDDLENAME" );
+			System.out.println( "middle: " + middle );
+			if( middle == null )
+			{
+				middle = "";
+			}
 
-	        r1.updateString( "MIDDLENAME", middle + "B" );
-	        r1.updateRow();
-	      }
-	      
-			Statement s2 = conn.createStatement(
-			        ResultSet.TYPE_SCROLL_SENSITIVE,
-			        ResultSet.CONCUR_UPDATABLE);
-			s2.executeQuery( "SELECT * FROM Allergy" );
-		      ResultSet r2 = s2.getResultSet();
-		      while (r2.next())
-		      {
-		        String code = r2.getString( "CODE" );
-		        System.out.println( "code: " + code );
-		        if( code == null ) {
-		        	code = "";
-		        }
+			r1.updateString( "MIDDLENAME", middle + "B" );
+			r1.updateRow();
+		}
 
-		        r2.updateString( "CODE", code + "B" );
-		        r2.updateRow();
-		      }
+		Statement s2 = conn.createStatement( ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE );
+		s2.executeQuery( "SELECT * FROM Allergy" );
+		ResultSet r2 = s2.getResultSet();
+		while( r2.next() )
+		{
+			String code = r2.getString( "CODE" );
+			System.out.println( "code: " + code );
+			if( code == null )
+			{
+				code = "";
+			}
 
-	      conn.commit();
-	      s1.close();
-	      
-	      conn.close();
+			r2.updateString( "CODE", code + "B" );
+			r2.updateRow();
+		}
+
+		Statement s3 = conn.createStatement( ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE );
+		s3.executeQuery( "select * from patient p, patientkey k where p.patientkey_id = k.id;" );
+		ResultSet r3 = s3.getResultSet();
+		while( r3.next() )
+		{
+			String lang = r3.getString( "LANGUAGE" );
+			System.out.println( "language: " + lang );
+			if( lang == null )
+			{
+				lang = "";
+			}
+
+			r3.updateString( "LANGUAGE", lang + "B" );
+			r3.updateRow();
+		}
+		
+		conn.commit();
+		s3.close();
+		s2.close();
+		s1.close();
+
+		
+		
+		conn.close();
 
 	}
 
