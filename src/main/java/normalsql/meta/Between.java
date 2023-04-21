7aab31b548635f490ba31ca2e2de5a0d62024908
jasonosgood <jason@jasonosgood.com>
2023-04-21 04:38:45 -0700

    javadoc

--- src/main/java/normalsql/meta/Between.java
+++ src/main/java/normalsql/meta/Between.java
@@ -6,24 +6,48 @@ package normalsql.meta;
 import normalsql.parse.NormalSQLParser.PredicateBETWEENContext;
 import normalsql.parse.NormalSQLParser.SubtermContext;
 
+/**
+ * Needed bits from parse tree of a BETWEEN clause
+ */
 public class
 	Between
 extends
 	Predicate<PredicateBETWEENContext>
 {
+	/**
+	 * enum of clause patterns matched (recognized)
+	 */
 	public enum Match
 	{
-		COL_VAL_VAL( COL, VAL, VAL ),  // eg 'column BETWEEN 0 AND 10'
+		/**
+		 * eg 'column BETWEEN 0 AND 10'
+		 */
+		COL_VAL_VAL( COL, VAL, VAL ),
 //		COL_COL_VAL( COL, COL, VAL ),  // eg 'column BETWEEN low AND 10'
 //		COL_VAL_COL( COL, VAL, COL ),  // eg 'column BETWEEN 0 AND high'
-		VAL_COL_COL( VAL, COL, COL ),  // eg '5 BETWEEN low AND high'
+		/**
+		 * eg '5 BETWEEN low AND high'
+		 */
+		VAL_COL_COL( VAL, COL, COL ),
 //		VAL_COL_VAL( VAL, COL, VAL ),  // eg '5 BETWEEN low AND 10'
 //		VAL_VAL_COL( VAL, VAL, COL ),  // eg '5 BETWEEN 0 AND high'
 //		VAL_VAL_VAL( VAL, VAL, VAL ),  // eg '5 BETWEEN 0 AND 10'
+		/**
+		 * no match
+		 */
 		NotMatched( null, null, null );
 
+		/**
+		 * The Left.
+		 */
 		public final Class left;
+		/**
+		 * The Low.
+		 */
 		public final Class low;
+		/**
+		 * The High.
+		 */
 		public final Class high;
 
 		Match( Class left, Class low, Class high )
@@ -33,6 +57,14 @@ extends
 			this.high = high;
 		}
 
+		/**
+		 * Match match.
+		 *
+		 * @param left the left
+		 * @param low  the low
+		 * @param high the high
+		 * @return the match
+		 */
 		public static Match match( SubtermContext left, SubtermContext low, SubtermContext high )
 		{
 			for( Match p : values() )
@@ -43,11 +75,28 @@ extends
 		}
 	}
 
-	public Match match;
-	public SubtermContext test;
-	public SubtermContext low;
-	public SubtermContext high;
+	/**
+	 * The Match.
+	 */
+	public final Match match;
+	/**
+	 * The Test.
+	 */
+	public final SubtermContext test;
+	/**
+	 * The Low.
+	 */
+	public final SubtermContext low;
+	/**
+	 * The High.
+	 */
+	public final SubtermContext high;
 
+	/**
+	 * Instantiates a new Between.
+	 *
+	 * @param context the context
+	 */
 	public Between( PredicateBETWEENContext context )
 	{
 		super( context );
