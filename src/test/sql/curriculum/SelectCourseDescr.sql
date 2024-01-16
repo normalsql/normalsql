c58674af43bf809af54dcd68b7257dbc527ea3d5
Jason Osgood <jason@jasonosgood.com>
2024-01-15 18:52:25 -0800

    housecleaning

--- src/test/sql/curriculum/SelectCourseDescr.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id, lastATP, course_title as title, desc, revision
-FROM Course c
-WHERE abbrev = 'ENGL'
-  AND number = 100
-ORDER BY lastATP desc;
--- src/test/sql/mysql/CalciteCTEFails.sql
+++
@@ -1,3 +0,0 @@
-with t1(x, y) as (select * from (values (1, 10), (2, 20), (cast(null as integer), 30)) as t),
-     t2(x, y) as (select * from (values (1,100), (cast(null as integer), 200)) as t)
-select * from t1 full join t2 on t1.x = t2.x;
