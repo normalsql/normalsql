443bc52926cd2ef2dceda39f2e03abeb0f48746b
Jason Osgood <jason@jasonosgood.com>
2024-07-04 09:05:13 -0700

    snapshot

--- build.md
+++ build.md
@@ -1,3 +1,23 @@
 Notes on how to build normalsql.
 
 mvn -e -X -D settings.xml package gpg:sign
+
+mvn -e -X -D settings.xml -Dmaven.test.skip=true clean compile package gpg:sign
+
+
+curl -O https://repo1.maven.org/maven2/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar
+
+
+curl -O \
+https://repo1.maven.org/maven2/net/sf/jopt-simple/jopt-simple/5.0.4/jopt-simple-5.0.4.jar
+
+https://repo1.maven.org/maven2/net/sf/jopt-simple/jopt-simple/5.0.1/
+
+<groupId>net.sf.jopt-simple</groupId>
+<artifactId>jopt-simple</artifactId>
+<version>5.0.4</version>
+
+./project/deps
+    runtime
+    test
+    compile
---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+#semver number
+
+#build number
+
+#imprint
+
+# clean
+rm -R ./simple
+mkdir simple
+mkdir simple/generated
+mkdir simple/out
+
+# dependencies ?
+
+# replace with build dirs
+
+
+#antlr
+java -cp "./lib/*" org.antlr.v4.Tool -package normalsql.parse -no-listener -visitor -o ./simple/generated -package normalsql.parse ./src/main/antlr4/normalsql/parse/NormalSQL.g4
+
+
+#javac
+javac -deprecation -g -parameters -source 21 -target 21 \
+ -version -verbose -Werror \
+ -cp "./lib/*" simple/**/*.java src/main/java/**/*.java -d simple/out
+
+
+# jar
+jar cvfm gorp.jar src/main/resources/META-INF/MANIFEST.MF -C simple/out . -C src/main/resources .
+
+# test
+
+# sign gpg
+
+# upload
