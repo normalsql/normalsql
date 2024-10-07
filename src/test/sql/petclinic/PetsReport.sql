f1faad965ab0044b19af4b047caa63e2ec481ff0
jasonosgood <jason@jasonosgood.com>
2024-10-07 16:08:36 -0700

    snapshot

--- src/test/sql/petclinic/PetsReport.sql
+++ src/test/sql/petclinic/PetsReport.sql
@@ -1,4 +1,4 @@
-   SELECT o.Last_Name AS Last, p.Name AS First, t.Name AS Species, Birth_Date AS DOB
+   SELECT o.Last_Name AS Last2, p.Name AS First2, t.Name AS Species2, Birth_Date AS DOB2
      FROM Owners o
-LEFT JOIN Pets p ON O.Id = p.Owner_Id
-LEFT JOIN Types t ON P.Type_Id = T.Id;
+LEFT JOIN Pets p ON o.Id = p.Owner_Id
+LEFT JOIN Types t ON p.Type_Id = T.Id;
