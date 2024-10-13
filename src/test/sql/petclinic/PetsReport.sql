dedc52800485dd27971c61f28b072206235c798b
Jason Osgood <jason@jasonosgood.com>
2024-10-13 10:20:49 -0700

    snapshot

--- src/test/sql/petclinic/PetsReport.sql
+++
@@ -1,5 +0,0 @@
-   SELECT o.Last_Name AS Last2, p.Name AS First2, t.Name AS Species2, Birth_Date AS DOB2
-     FROM Owners o
-LEFT JOIN Pets p ON o.Id = p.Owner_Id
-LEFT JOIN Types t ON p.Type_Id = T.Id
-    WHERE o.Last_Name ILIKE 'Mc%';
